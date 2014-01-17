657a088efe67a9f7f5b0e63ff2aea9baf7c73b2b
csteipp <csteipp@wikimedia.org>
2014-01-16 16:35:33 -0800

    Browser Tests for MW 1.19

---
+++ browsertests_119/features/step_definitions/login_steps.rb
@@ -0,0 +1,55 @@
+#
+# This file is subject to the license terms in the LICENSE file found in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/LICENSE. No part of
+# qa-browsertests, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE file.
+#
+# Copyright 2012-2013 by the Mediawiki developers. See the CREDITS file in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/CREDITS
+#
+Given(/^I am at Log in page$/) do
+  visit LoginPage
+end
+
+When(/^I log in with incorrect password$/) do
+  on(LoginPage).login_with(ENV["MEDIAWIKI_USER"], "incorrect password")
+end
+When(/^I log in with incorrect username$/) do
+  on(LoginPage).login_with("incorrect username", ENV["MEDIAWIKI_PASSWORD"])
+end
+When(/^I log in without entering credentials$/) do
+  on(LoginPage).login_with("", "")
+end
+When(/^I log in without entering password$/) do
+  on(LoginPage).login_with(ENV["MEDIAWIKI_USER"], "")
+end
+When(/^Log in as (.+)$/) do |username|
+  on(LoginPage).login_with(username, ENV["MEDIAWIKI_PASSWORD"])
+end
+
+Then(/^feedback should be (.+)$/) do |feedback|
+    on(LoginPage) do |page|
+      page.feedback_element.when_present.click
+      page.feedback.should match Regexp.escape(feedback)
+    end
+  end
+Then(/^Log in element should be there$/) do
+  on(LoginPage).login_element.should exist
+end
+Then(/^Log in page should open$/) do
+  @browser.url.should match Regexp.escape("Special:UserLogin")
+end
+Then(/^main page should open$/) do
+  @browser.url.should == on(MainPage).class.url
+end
+Then(/^Password element should be there$/) do
+  on(LoginPage).password_element.should exist
+end
+Then(/^there should be a link to (.+)$/) do |text|
+  on(LoginPage).username_displayed_element.when_present.text.should == text
+end
+Then(/^Username element should be there$/) do
+  on(LoginPage).username_element.should exist
+end
---
+++ browsertests_119/features/support/pages/random_page.rb
@@ -0,0 +1,44 @@
+#
+# This file is subject to the license terms in the LICENSE file found in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/LICENSE. No part of
+# qa-browsertests, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE file.
+#
+# Copyright 2012-2013 by the Mediawiki developers. See the CREDITS file in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/CREDITS
+#
+class RandomPage
+  include PageObject
+
+  include URL
+  page_url URL.url("Special:Random")
+
+  span(:cog, title: "Language settings")
+  a(:community_portal, text: "Community portal")
+  a(:create_a_book, text: "Create a book")
+  a(:download_as_pdf, text: "Download as PDF")
+  a(:download_the_file, text: "Download the file")
+  a(:edit_page, title: /You can edit this page/)
+  text_area(:editor_text, id: "wpTextbox1")
+  div(:input_method, class: "imeselector imeselector-toggle")
+  a(:input_method_enabled, class: "ime-name imeselector-toggle")
+  h3(:input_method_ime_list_title, class: "ime-list-title")
+  ul(:input_method_language_list, class: "ime-language-list")
+  div(:input_method_selector_menu, class: "imeselector-menu")
+  a(:interaction_section, text: "Interaction")
+  text_field(:language_filter, id: "languagefilter")
+  div(:language_settings_dialog, id: "language-settings-dialog")
+  li(:main_page, id: "n-mainpage-description")
+  a(:more_languages, class: "ime-selector-more-languages")
+  a(:print_export, text: "Print/export")
+  a(:printable_version, text: "Printable version")
+  li(:uls_malayalam_inscript2_item, data_ime_inputmethod: "ml-inscript2")
+  #button(:search_button, id: "searchButton")
+  button(:search_button, type: "submit", value: "Search")
+  text_field(:search_input, id: "searchInput")
+  a(:tools, text: "Tools")
+  a(:uls_trigger, class: "uls-trigger")
+  a(:upload_file, text: "Upload file")
+end
